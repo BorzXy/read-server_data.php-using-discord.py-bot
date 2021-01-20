5e9ac22f1342c055acb683b8a5fc2940ca19c902
ImDanilo <66525747+daniloanantaa@users.noreply.github.com>
2021-01-20 09:15:13 +0800

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,28 @@
+#reader discord.py made by ImDanilo & BorzXy
+#Inspired by ClayneID (https://github.com/ClayneID)
+import discord
+from discord.ext import commands
+import requests
+
+daniloboi = commands.Bot(command_prefix=">")
+
+@daniloboi.event
+async def on_ready():
+	print('Bot Made by ImDanilo & BorzXy')
+	print(f"-----\nLogged in as: {daniloboi.user.name} : {daniloboi.user.id}\n-----")
+	print("Error Logs:")
+	activity = discord.Game(name="Playing Reader By ImDanilo#9999 & BorzXy#0001", type=3)
+	await daniloboi.change_presence(activity=discord.Game(name="bot made by BorzXy#0001 & ImDanilo#9999"))
+
+@daniloboi.command()
+async def test(ctx):
+	await ctx.send('Bot Already Online Type >read <IP> to read someone server server_data.php')
+
+@daniloboi.command()
+async def read(ctx, message):
+    await ctx.send('Wait 10 sec...')
+    reader = requests.post(f'http://{message}/growtopia/server_data.php')
+    print(reader)
+    await ctx.send(reader.text)
+
+daniloboi.run('Nzg3Njg1NzY4MDEyNjI3OTg4.X9YjiQ.3Ut5NMYkUfdM4m4p7HCoA9JFXJE')
