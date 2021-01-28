7ee64c04ec159f8f6fefd73c231548b867a598f2
BorzXy <66525747+daniloanantaa@users.noreply.github.com>
2021-01-28 11:05:50 +0800

    Update
    #Update 1 css text
    #Update 2 Embed text

--- bot.py
+++ bot.py
@@ -19,12 +19,22 @@ async def test(ctx):
 	await ctx.send('Bot Already Online Type >read <IP> to read someone server server_data.php')
 
 @daniloboi.command()
-async def read(ctx, message):
+async def reader(ctx, message):
     await ctx.message.delete()
     await ctx.send('Please Wait...')
     reader = requests.post(f'http://{message}/growtopia/server_data.php')
     print(reader)
     await ctx.send(f'```css\n{reader.text}\n```')
 #Update 1
+@daniloboi.command()
+async def read(ctx, message):
+    await ctx.message.delete()
+    await ctx.send('Please Wait...')
+    reader = requests.post(f'http://{message}/growtopia/server_data.php')
+    print(reader)
+    read = discord.Embed(title=f"{message} Result:", colour=000000, timestamp=ctx.message.created_at)
+    read.add_field(name=f"Result:", value= f'```\n{reader.text}\n```')
+    await ctx.send(embed=read)
+#Update 2 embed
 
 daniloboi.run('Your bot token')
